dcb687cee88eddc13a356e000d90952e75638944
Jan Lelis <mail@janlelis.de>
2016-01-29 15:44:00 +0100

    redux in 19 lines

--- lib/redux.rb
+++ lib/redux.rb
@@ -1,5 +1,25 @@
 require_relative "redux/version"
 
 module Redux
+  class Store
+    attr_reader :state
+
+    def initialize(&reducer)
+      @state     = nil
+      @reducer   = reducer || ->{}
+      @listeners = []
+      dispatch({})
+    end
+
+    def dispatch(action)
+      @state = @reducer.call(@state, action)
+      @listeners.each{ |listener| listener.call() }
+    end
+
+    def subscribe(&listener)
+      @listeners << listener
+      ->{ @listeners.delete(listener) }
+    end
+  end
 end
 
