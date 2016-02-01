3beef484b17fb9d65cb8983f269c7ded988f08c3
Jan Lelis <mail@janlelis.de>
2016-02-01 14:55:09 +0100

    draft combine reducers

--- lib/redux.rb
+++ lib/redux.rb
@@ -1,2 +1,12 @@
 require_relative "redux/version"
 require_relative "redux/store"
+
+module Redux
+  def self.combine_reducers(reducers)
+    ->(state = {}, action){
+      reducers.reduce({}){ |next_state, (key, reducer)|
+        next_state[key] = reducer.call(state[key], action)
+      }
+    }
+  end
+end
